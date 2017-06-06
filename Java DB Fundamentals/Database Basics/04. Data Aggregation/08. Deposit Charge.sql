d861a3d8f06d50ddc083c226d6349a6cafec71e4
Ivan Menkachev <ivan_menkachev@abv.bg>
2017-06-07 01:38:40 +0300

    Add "08. Deposit Charge".

---
+++ Java DB Fundamentals/Database Basics/04. Data Aggregation/08. Deposit Charge.sql
@@ -0,0 +1,7 @@
+SELECT 
+    wd.`deposit_group`,
+    wd.`magic_wand_creator`,
+    MIN(wd.`deposit_charge`) AS `min_deposit_charge`
+FROM
+    `wizzard_deposits` AS wd
+GROUP BY wd.`magic_wand_creator`, wd.`deposit_group`;
