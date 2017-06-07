1a545c6530fe5d759e636cd34f9d2fc79ac09414
Ivan Menkachev <ivan_menkachev@abv.bg>
2017-06-07 15:26:15 +0300

    Add "12. Rich Wizard, Poor Wizard".

---
+++ Java DB Fundamentals/Database Basics/04. Data Aggregation/12. Rich Wizard, Poor Wizard.sql
@@ -0,0 +1,16 @@
+CREATE VIEW `rw_pw` AS
+    SELECT 
+        wd1.`first_name` AS `host_wizard`,
+        wd1.`deposit_amount` AS `host_wizard_deposit`,
+        wd2.`first_name` AS `guest_wizard`,
+        wd2.`deposit_amount` AS `guest_wizard_deposit`
+    FROM
+        `wizzard_deposits` AS wd1,
+        `wizzard_deposits` AS wd2
+    WHERE
+        wd1.`id` + 1 = wd2.`id`;
+        
+SELECT 
+    SUM(`host_wizard_deposit` - `guest_wizard_deposit`) AS `sum_difference`
+FROM
+    `rw_pw`;
