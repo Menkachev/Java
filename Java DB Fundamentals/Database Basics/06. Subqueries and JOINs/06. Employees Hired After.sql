12782c22c5bd493b02ed7b247765cd67c36ea5fc
Ivan Menkachev <ivan_menkachev@abv.bg>
2017-06-14 18:54:25 +0300

    Add "06. Employees Hired After".

---
+++ Java DB Fundamentals/Database Basics/06. Subqueries and JOINs/06. Employees Hired After.sql
@@ -0,0 +1,10 @@
+SELECT 
+    e.`first_name`, e.`last_name`, e.`hire_date`, d.`name` AS 'department_name'
+FROM
+    `employees` AS e
+        INNER JOIN
+    `departments` AS d ON e.`department_id` = d.`department_id`
+WHERE
+    e.`hire_date` > '1999-1-1 23-59-59'
+        AND d.`name` IN ('Sales' , 'Finance')
+ORDER BY e.`hire_date`;
