5f15e3e5e78f2dccc9dbcf7568edf505672a7ff5
Ivan Menkachev <ivan_menkachev@abv.bg>
2017-06-21 01:50:52 +0300

    Add "06. Customers".

---
+++ Java DB Fundamentals/Database Basics/DB MySQL Exam Preparation I/06. Customers.sql
@@ -0,0 +1,7 @@
+SELECT 
+    c.`customer_id`,
+    CONCAT(c.`first_name`, ' ', c.`last_name`) AS `full_name`,
+    c.`gender`
+FROM
+    `customers` AS c
+ORDER BY `full_name` ASC , c.`customer_id` ASC;
