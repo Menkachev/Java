f089cbb084b0d7c169f9ec686f2ea221c7f34630
Ivan Menkachev <ivan_menkachev@abv.bg>
2017-07-21 20:38:03 +0300

    Add Hotel Database.

---
+++ Java DB Fundamentals/Databases Advanced/03. Hibernate Code First & Entity Relations/Hotel Database/src/main/java/Entities/BedTypes.java
@@ -0,0 +1,43 @@
+package Entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "bed_types")
+public class BedTypes {
+
+    private Long id;
+    private String bedType;
+    private String notes;
+
+    public BedTypes() {
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Column(name = "bed_type")
+    public String getBedType() {
+        return bedType;
+    }
+
+    public void setBedType(String bedType) {
+        this.bedType = bedType;
+    }
+
+    @Column(name = "notes")
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+}
